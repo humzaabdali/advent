0ad96c8b92ce48f69db2bfb60c27b114a401a8dc
Matthew <92883652+CodeMaestro42@users.noreply.github.com>
2026-03-21 20:43:33 -0700

    Create accumulator.sv

---
+++ accumulator.sv
@@ -0,0 +1,13 @@
+module accumulator(input, clk, input rst, input [39:0] vector, input i_invalid, output reg [63:0] accumulated_id);
+    
+    always@(posedge clk) begin 
+        if (rst) begin 
+            accumulated_id < =64'd0;
+        end 
+        else begin 
+            if (i_invalid) begin 
+                accumulated_id<=accumulated_id+vector;
+            end 
+        end 
+    end 
+endmodule
