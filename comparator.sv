48b50154bafb2d7a4d1e3ef05cc5278704a41013
Matthew <92883652+CodeMaestro42@users.noreply.github.com>
2026-03-28 19:15:11 -0700

    Create comparator.sv
    updated comparator with pass through bcd vector

---
+++ comparator.sv
@@ -0,0 +1,22 @@
+module comparator(
+  input [39:0] i_bcd_vector,
+    input [3:0]  leading_zeros,
+    input        i_valid,
+    output reg   is_invalid_id,
+    output reg   o_valid,
+    output reg [39:0]   o_bcd_vector
+);
+
+    always @(*) begin
+        case (leading_zeros)
+            4'd0: is_invalid_id = (bcd_vector[39:20] == bcd_vector[19:0]);
+            4'd2: is_invalid_id = (bcd_vector[31:16] == bcd_vector[15:0]);
+            4'd4: is_invalid_id = (bcd_vector[23:12] == bcd_vector[11:0]);
+            4'd6: is_invalid_id = (bcd_vector[15:8]  == bcd_vector[7:0]);
+            4'd8: is_invalid_id = (bcd_vector[7:4]   == bcd_vector[3:0]);
+            default: is_invalid_id = 1'b0;
+        endcase
+
+        o_valid = i_valid;
+        o_bcd_vector = i_bcd_vector;
+    end
