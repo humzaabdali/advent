83148b0334113f25d1c7413703ca20b17055b96e
Matthew <92883652+CodeMaestro42@users.noreply.github.com>
2026-05-07 01:20:02 -0700

    max_finder

---
+++ max_finder.sv
@@ -0,0 +1,41 @@
+module max_finder(
+    input[39:0] i_BCD_vector,
+    input[3:0] i_leading_zeros,
+    output reg [7:0] max_vector,
+    input clk,
+    input i_valid,
+    output o_valid,
+    input reset
+);
+
+reg[2:0] state_reg, next_state_reg;
+
+parameter idle = 3'b000, start =3'b001, shift = 3'b010, compare = 3'b011, finish =3'b100;
+
+always@(posedge clk or posedge) begin
+    if (reset) begin 
+        state_reg<=idle;
+    end 
+    else begin 
+        state_reg<=next_state_reg;
+    end 
+end 
+
+always@(*) begin 
+    case (state_reg)
+    idle: begin 
+    end
+    start: begin 
+    end 
+    shift: begin 
+    end 
+    compare: begin
+    end 
+    finish: begin 
+    end 
+    endcase
+end 
+
+assign o_valid = i_valid;
+
+endmodule
