8905fe67b332513676fc6aa07ee33a076de993f5
Matthew <92883652+CodeMaestro42@users.noreply.github.com>
2026-05-07 18:42:36 -0700

    accumulator finished

--- BCD_accumulator.sv
+++ BCD_accumulator.sv
@@ -1,8 +1,15 @@
 module BCD_accum(
 input [7:0] i_max_vector,
 input i_valid,
-output reg [15:0] o_accumulated_sum
+output reg [32:0] o_accumulated_sum
 );
+
+always@(*) begin 
+if (i_valid)
+    o_accumulated_sum = o_accumulated_sum+i_max_vector;
+    else 
+    o_accumulated_sum = o_accumulated_sum;
+end 
 
 
 endmodule
