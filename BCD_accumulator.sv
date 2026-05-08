376fbd4bfabce7bcb5456e9d49d666e4547d8074
Matthew <92883652+CodeMaestro42@users.noreply.github.com>
2026-05-07 18:43:33 -0700

    fixed accumulator

--- BCD_accumulator.sv
+++ BCD_accumulator.sv
@@ -1,15 +1,16 @@
 module BCD_accum(
 input [7:0] i_max_vector,
 input i_valid,
-output reg [32:0] o_accumulated_sum
+output reg [31:0] o_accumulated_sum,
+input clk
 );
 
-always@(*) begin 
-if (i_valid)
-    o_accumulated_sum = o_accumulated_sum+i_max_vector;
-    else 
-    o_accumulated_sum = o_accumulated_sum;
-end 
+    always @(posedge clk) begin
+        if (rst)
+            o_accumulated_sum <= 32'd0;
+        else if (i_valid)
+            o_accumulated_sum <= o_accumulated_sum + i_max_vector;
+    end
 
 
 endmodule
